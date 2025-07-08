8c7969174e1a65c4ec2032b48f3c26b96e4e85b3
9405792200 <76832809+9405792200@users.noreply.github.com>
2025-07-08 21:44:41 +0530

    Add files via upload

---
+++ 43_special_variable_name/demo.py
@@ -0,0 +1,5 @@
+from calc import * 
+
+if __name__ == "__main__": # __name__ will be "__main__" if this file execute standalone, 
+                          # if it is imported from another module then __name__ will be module which is demo  
+    print("Welcome to Demo")
